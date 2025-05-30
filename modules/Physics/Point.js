50c916a93820675ef8e47eb189ce025ece8927f3
zsh-dre <zsh.dre@gmail.com>
2025-05-30 10:05:17 -0300

    Create Physics/Point.js
    Handles basic points

---
+++ modules/Physics/Point.js
@@ -0,0 +1,17 @@
+class Point {
+  x, y;
+  
+  constructor(x=0, y=0)
+  {
+    this.x=x;
+    this.y=y;
+  }
+  
+  static displayName = "Point";
+  static distance(a, b)
+  {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+    return Math.hypot(dx, dy);
+  }
+}
